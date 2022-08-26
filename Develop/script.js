4633db0d7996494fd54eca68194ca193604699c9
chayannesteps <109673257+chayannesteps@users.noreply.github.com>
2022-08-25 22:51:35 -0400

    time display attempt

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,9 @@
-// var dateTime = new Date();
-// console.log(dateTime);
-// document.getElementById("time").textContent = dateTime
-
-// `use strict`;
-// function refreshTime() {
-//   const timeDisplay = document.getElementById("time");
-//   const dateString = new Date().toLocaleString();
-//   const formattedString = dateString.replace(", ", " - ");
-//   timeDisplay.textContent = formattedString;
-}
-//   setInterval(refreshTime, 1000);
-
-  const colorcode = document.getElementsByClassName("colorcode");
-let currentHour = Date.now().getHours();//get current time hour
-
-colorcode.forEach(function(colorcodedDiv){
-    let colorcodeHour = parseInt(colorcodedDiv.id);
-  if (colorcodeHour) {
-    if (currentHour === colorcodeHour) {
-      setColor(colorcodedDiv, "Red");
-    } else if ((currentHour < colorcodeHour) && (currentHour > colorcodeHour - 6)) {
-      setColor(colorcodedDiv, "Green");
-    } else if ((currentHour > colorcodeHour) && (currentHour < colorcodeHour + 6)) {
-      setColor(colorcodedDiv, "LightGrey");
-    } else {
-      setColor(colorcodedDiv, "White");
-    }
-  }
-});
-function setColor(element, color) {
-    element.style.backgroundColor = color;
-}
+// attempt to display time//
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+ 
+// document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
